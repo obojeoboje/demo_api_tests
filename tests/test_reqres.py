a0d92947e20ef326a65ef817a9b0bad017b07f27
Aleksandr Privalov <obojealexander@gmail.com>
2023-04-05 17:47:11 +0300

    added allure suits and severity

--- tests/test_reqres.py
+++ tests/test_reqres.py
@@ -1,5 +1,6 @@
 import requests
 import allure
+from allure_commons.types import Severity
 from requests import Response
 from pytest_voluptuous import S
 from shemas.reqres import get_single_user_schema, \
@@ -9,6 +10,10 @@ from shemas.reqres import get_single_user_schema, \
 from models.helpers import base_url, data_create_user, data_edit_user, data_register_user
 
 
+@allure.parent_suite("API")
+@allure.severity(Severity.CRITICAL)
+@allure.suite("User")
+@allure.title("Get single user")
 def test_get_single_user():
     with allure.step('send get request'):
         response: Response = requests.get(base_url + "/api/users/2")
@@ -23,7 +28,10 @@ def test_get_single_user():
         assert response.json()['data']['first_name'] == 'Janet'
         assert response.json()['data']['last_name'] == 'Weaver'
 
-
+@allure.parent_suite("API")
+@allure.severity(Severity.MINOR)
+@allure.suite("User")
+@allure.title("Post single user")
 def test_post_create_user():
     with allure.step('send post request'):
         create = requests.post(base_url + "/api/users", data_create_user())
@@ -38,7 +46,10 @@ def test_post_create_user():
         assert isinstance(create.json()["id"], str)
         assert isinstance(create.json()["createdAt"], str)
 
-
+@allure.parent_suite("API")
+@allure.severity(Severity.TRIVIAL)
+@allure.suite("User")
+@allure.title("Put single user")
 def test_put_user():
     with allure.step('send put request'):
         update = requests.put(base_url + '/api/users/2', data_edit_user())
@@ -51,7 +62,10 @@ def test_put_user():
         assert update.json()['name'] == data_edit_user()['name']
         assert update.json()['job'] == data_edit_user()['job']
 
-
+@allure.parent_suite("API")
+@allure.severity(Severity.CRITICAL)
+@allure.suite("User")
+@allure.title("Delete single user")
 def test_delete_user():
     with allure.step('send delete request'):
         result = requests.delete('https://reqres.in/api/users/2')
@@ -60,6 +74,10 @@ def test_delete_user():
         assert result.status_code == 204
 
 
+@allure.parent_suite("API")
+@allure.severity(Severity.NORMAL)
+@allure.suite("User")
+@allure.title("Post register single user")
 def test_post_register_user():
     with allure.step('send post request'):
         register = requests.post(base_url + '/api/register', data_register_user())
